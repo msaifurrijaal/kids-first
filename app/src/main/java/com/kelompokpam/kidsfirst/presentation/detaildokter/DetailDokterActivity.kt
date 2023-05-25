49fc1ffe91d203663c210857c1482021dae0bb00
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2023-05-25 21:30:14 +0700

    menambahkan halaman detail dokter

---
+++ app/src/main/java/com/kelompokpam/kidsfirst/presentation/detaildokter/DetailDokterActivity.kt
@@ -0,0 +1,12 @@
+package com.kelompokpam.kidsfirst.presentation.detaildokter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kelompokpam.kidsfirst.R
+
+class DetailDokterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_dokter)
+    }
+}
