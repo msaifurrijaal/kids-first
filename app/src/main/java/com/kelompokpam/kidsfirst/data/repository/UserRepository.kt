e8de4b4f22c6fa6da5a80e451b75214857720000
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2023-05-27 19:50:33 +0700

    menambahkan fungsi mengambil data list dokter

--- app/src/main/java/com/kelompokpam/kidsfirst/data/repository/UserRepository.kt
+++ app/src/main/java/com/kelompokpam/kidsfirst/data/repository/UserRepository.kt
@@ -5,8 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.kelompokpam.kidsfirst.data.Resource
 import com.kelompokpam.kidsfirst.data.model.User
 
@@ -80,6 +83,32 @@ class UserRepository(application: Application) {
             authResult.value = Resource.Success(it)
         }
         return authResult
+    }
+
+    fun getDokter(): LiveData<Resource<List<User>>> {
+        val dokterLiveData = MutableLiveData<Resource<List<User>>>()
+        dokterLiveData.value = Resource.Loading()
+
+        userDatabase.orderByChild("role_user").equalTo("dokter")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val userList = mutableListOf<User>()
+
+                    for (userSnapshot in dataSnapshot.children) {
+                        val user = userSnapshot.getValue(User::class.java)
+                        user?.let {
+                            userList.add(it)
+                        }
+                    }
+
+                    dokterLiveData.value = Resource.Success(userList)
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    dokterLiveData.value = Resource.Error(databaseError.message)
+                }
+            })
+        return dokterLiveData
     }
 
 }
---
+++ app/src/main/java/com/kelompokpam/kidsfirst/presentation/home/BerandaViewModel.kt
@@ -0,0 +1,12 @@
+package com.kelompokpam.kidsfirst.presentation.home
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.kelompokpam.kidsfirst.data.repository.UserRepository
+
+class BerandaViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository = UserRepository(application)
+
+    fun getListDokter() = repository.getDokter()
+}
