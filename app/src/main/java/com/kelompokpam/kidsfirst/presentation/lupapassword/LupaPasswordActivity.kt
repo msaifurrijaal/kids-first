88edacd28cfac70b7642d894dd4467e142c500cf
Natabagas <meganatabagas@gmail.com>
2023-05-30 15:36:38 +0700

    menambahkan halaman lupa password

---
+++ app/src/main/java/com/kelompokpam/kidsfirst/presentation/lupapassword/LupaPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.kelompokpam.kidsfirst.presentation.lupapassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kelompokpam.kidsfirst.R
+
+class LupaPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lupa_password)
+    }
+}
