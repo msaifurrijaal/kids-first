65540f29bfebf7d52fcaf7340b8ad68d4bc7ee8c
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2023-06-03 13:19:57 +0700

    menambahkan fitur camera untuk mengambil gambar pada EditActivity

--- app/src/main/java/com/kelompokpam/kidsfirst/presentation/editprofil/EditProfilActivity.kt
+++ app/src/main/java/com/kelompokpam/kidsfirst/presentation/editprofil/EditProfilActivity.kt
@@ -1,19 +1,34 @@
 package com.kelompokpam.kidsfirst.presentation.editprofil
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.kelompokpam.kidsfirst.R
 import com.kelompokpam.kidsfirst.data.Resource
 import com.kelompokpam.kidsfirst.data.model.User
 import com.kelompokpam.kidsfirst.databinding.ActivityEditProfilBinding
+import com.kelompokpam.kidsfirst.utils.createCustomTempFile
 import com.kelompokpam.kidsfirst.utils.showDialogError
 import com.kelompokpam.kidsfirst.utils.showDialogLoading
 import com.kelompokpam.kidsfirst.utils.showDialogSuccess
+import java.io.File
 
 class EditProfilActivity : AppCompatActivity() {
 
@@ -21,6 +36,8 @@ class EditProfilActivity : AppCompatActivity() {
     private lateinit var editProfilViewModel: EditProfilViewModel
     private lateinit var dialogLoading: AlertDialog
     private var user: User? = null
+    private var imageUser: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfilBinding.inflate(layoutInflater)
@@ -30,11 +47,21 @@ class EditProfilActivity : AppCompatActivity() {
         dialogLoading = showDialogLoading(this)
 
         getInformationFromIntent()
+        setInformationFromIntent()
         onAction()
     }
 
     private fun onAction() {
         binding.apply {
+
+            ivUser.setOnClickListener {
+                if (!allPermissionsGranted()) {
+                    requestCameraPermission()
+                } else if (allPermissionsGranted()) {
+                    startTakePhoto()
+                }
+            }
+
             btnCloseEditProfil.setOnClickListener {
                 finish()
             }
@@ -73,6 +100,76 @@ class EditProfilActivity : AppCompatActivity() {
         }
     }
 
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@EditProfilActivity,
+                "com.kelompokpam.kidsfirst",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+
+            imageUser =  BitmapFactory.decodeFile(myFile.path)
+
+            binding.ivUser.setImageBitmap(imageUser)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (allPermissionsGranted()) {
+                startTakePhoto()
+            }
+        }
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            REQUIRED_PERMISSIONS,
+            REQUEST_CODE_PERMISSIONS
+        )
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun setInformationFromIntent() {
+        binding.apply {
+            Glide.with(this@EditProfilActivity)
+                .load(user?.avatarUser)
+                .into(ivUser)
+
+            etNamaBaru.setText(user?.nameUser)
+        }
+    }
+
     private fun getInformationFromIntent() {
         user = intent.getParcelableExtra<User>(EditProfilActivity.USER_ITEM)
         Log.d("EditProfilActivity", user.toString())
@@ -80,6 +177,8 @@ class EditProfilActivity : AppCompatActivity() {
 
     companion object {
         const val USER_ITEM = "user_item"
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 
 }
